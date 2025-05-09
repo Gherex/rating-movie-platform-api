033437d3dca34ecdd2a69b2a28fa3f3ffdfc4c9c
Gherex <germilagger@gmail.com>
2025-05-09 04:18:50 -0300

    First commit

---
+++ src/test/java/com/gherex/ratingmovieplatform/RatingMoviePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gherex.ratingmovieplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingMoviePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
